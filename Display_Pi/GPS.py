ae8aeb3485d39369966f5326195ca552103f8e0c
Mokey2002 <edventura91@gmail.com>
2016-12-05 20:29:09 -0800

    Added Reference

--- Display_Pi/GPS.py
+++ Display_Pi/GPS.py
@@ -1,6 +1,5 @@
-#! /usr/bin/python
-# Written by Dan Mandle http://dan.mandle.me September 2012
-# License: GPL 2.0
+# Reference
+# http://www.danmandle.com/blog/getting-gpsd-to-work-with-python/
  
 import os
 from gps import *
@@ -42,28 +41,7 @@ if __name__ == '__main__':
       fcntl.flock(f, fcntl.LOCK_UN)
       f.close()
       print
-      print ' GPS reading'
-      print '----------------------------------------'
-      print 'latitude    ' , gpsd.fix.latitude
-      print 'longitude   ' , gpsd.fix.longitude
-      print 'time utc    ' , gpsd.utc,' + ', gpsd.fix.time
-      print 'altitude (m)' , gpsd.fix.altitude
-      print 'eps         ' , gpsd.fix.eps
-      print 'epx         ' , gpsd.fix.epx
-      print 'epv         ' , gpsd.fix.epv
-      print 'ept         ' , gpsd.fix.ept
-      print 'speed (m/s) ' , gpsd.fix.speed
       print 'MPH         ' , speed
-      print 'climb       ' , gpsd.fix.climb
-      print 'track       ' , gpsd.fix.track
-      print 'mode        ' , gpsd.fix.mode
-      print
-      print 'sats        ' , gpsd.satellites
  
       time.sleep(5) #set to whatever
  
-  except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
-    print "\nKilling Thread..."
-    gpsp.running = False
-    gpsp.join() # wait for the thread to finish what it's doing
-  print "Done.\nExiting."
